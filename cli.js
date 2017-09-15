74c177737b814462f97eb1f427bcef5c17294c47
Lufty Wiranda <lufty.wiranda@gmail.com>
2017-09-15 13:46:25 +0700

    fix: upgrade to `del-nm` v2 (#1)

--- cli.js
+++ cli.js
@@ -5,28 +5,20 @@ const delNm = require('del-nm');
 const meow = require('meow');
 const updateNotifier = require('update-notifier');
 
-const cli = meow(
-  `
+const cli = meow(`
 	Usage
 	  $ del-nm <path|glob>
 
 	Examples
-	  $ del-nm .
+	  $ del-nm
 	  $ del-nm foo
-	  $ del-nm foo/bar
-`,
-);
+	  $ del-nm ../bar
+`);
 
 updateNotifier({pkg: cli.pkg}).notify();
 
-(function () {
-  const paths = delNm(cli.input.toString());
-
-  if (paths.length !== 0) {
-    console.log('Deleted:');
-
-    paths.forEach(path => {
-      console.log(path);
-    });
-  }
-})();
+delNm(cli.input[0]).then(paths => {
+  paths.forEach(path => {
+    console.log(path);
+  });
+});
