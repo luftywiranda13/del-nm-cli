679d1548efe7791bf4e7815c7bce87106498fa8c
Lufty Wiranda <lufty.wiranda@gmail.com>
2017-09-20 11:42:34 +0700

    test: improve tests

--- test.js
+++ test.js
@@ -9,8 +9,12 @@ const tempy = require('tempy');
 test('CLI', async () => {
   const tempDir = tempy.directory();
   makeDir.sync(path.join(tempDir, 'node_modules'));
+  tempy.file({name: 'package-lock.json'});
+  tempy.file({name: 'yarn.lock'});
 
   await execa('./cli.js', [tempDir]);
 
   expect(fs.existsSync(path.join(tempDir, 'node_modules'))).toBe(false);
+  expect(fs.existsSync(path.join(tempDir, 'package-lock.json'))).toBe(false);
+  expect(fs.existsSync(path.join(tempDir, 'yarn.lock'))).toBe(false);
 });
