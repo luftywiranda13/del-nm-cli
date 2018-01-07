120a12165049cbd194859984b8652eb12aa04b91
Lufty Wiranda <lufty.wiranda@gmail.com>
2018-01-08 06:33:33 +0700

    test: remove unnecessary testing path existences

--- test.js
+++ test.js
@@ -1,24 +1,19 @@
 'use strict';
 
-const path = require('path');
-const fs = require('fs-extra');
 const execa = require('execa');
+const fs = require('fs-extra');
 const tempy = require('tempy');
 
-test('CLI', async () => {
-  expect.assertions(6);
+test('output', async () => {
+  expect.assertions(3);
   const tempDir = tempy.directory();
-  await fs.ensureDir(`${tempDir}/node_modules`);
-  await fs.ensureFile(`${tempDir}/yarn.lock`);
-  await fs.ensureFile(`${tempDir}/package-lock.json`);
+  await fs.mkdir(`${tempDir}/node_modules`);
+  await fs.createFile(`${tempDir}/yarn.lock`);
+  await fs.createFile(`${tempDir}/package-lock.json`);
 
   const stdout = await execa.stdout('./cli.js', [tempDir]);
 
   expect(stdout).toMatch(/node_modules/);
   expect(stdout).toMatch(/yarn.lock/);
   expect(stdout).toMatch(/package-lock.json/);
-
-  expect(fs.existsSync(path.join(tempDir, 'node_modules'))).toBe(false);
-  expect(fs.existsSync(path.join(tempDir, 'yarn.lock'))).toBe(false);
-  expect(fs.existsSync(path.join(tempDir, 'package-lock.json'))).toBe(false);
 });
