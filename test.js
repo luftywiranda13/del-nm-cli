328e62aaf211d7c7a466d2ebdeab53460468f789
Lufty Wiranda <lufty.wiranda@gmail.com>
2018-01-09 01:13:21 +0700

    fix: upgrade `del-nm`

--- test.js
+++ test.js
@@ -5,15 +5,17 @@ const fs = require('fs-extra');
 const tempy = require('tempy');
 
 test('output', async () => {
-  expect.assertions(3);
+  expect.assertions(4);
   const tempDir = tempy.directory();
   await fs.mkdir(`${tempDir}/node_modules`);
   await fs.createFile(`${tempDir}/yarn.lock`);
   await fs.createFile(`${tempDir}/package-lock.json`);
+  await fs.createFile(`${tempDir}/npm-shrinkwrap.json`);
 
   const stdout = await execa.stdout('./cli.js', [tempDir]);
 
   expect(stdout).toMatch(/node_modules/);
   expect(stdout).toMatch(/yarn.lock/);
   expect(stdout).toMatch(/package-lock.json/);
+  expect(stdout).toMatch(/npm-shrinkwrap.json/);
 });
